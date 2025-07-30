7e1f817bdbd6e249b2eee844b614f93e09be9de9
PrasunShrivastav <shrivastavprasun36@gmail.com>
2025-07-30 23:49:48 +0530

    fixed the path to frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use("/api/notes", notesRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 connectDB().then(() => {
